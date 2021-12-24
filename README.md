b63b74113241f28a49be2edfb0ceee79caaf04b8
Keverson Garcia <50189112+keversongarcia@users.noreply.github.com>
2021-12-24 14:02:53 -0300

    readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Spotify with new interface
 
 Started project!
+
+![Alt text](/assets/images/spotify_index.png?raw=true "Title")
--- src/views/index/Index.tsx
+++ src/views/index/Index.tsx
@@ -1,8 +1,20 @@
+import useSpotify from "@/hooks/useSpotify";
 import { Box, Flex } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 import NewReleases from "./components/NewReleases";
 import RecentlyTracks from "./components/RecentlyTracks";
 
 const Index = () => {
+  const { data: session } = useSession();
+  const spotifyApi = useSpotify();
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getMyDevices().then((data) => console.log(data?.body.devices));
+    }
+  }, [session, spotifyApi]);
+
   return (
     <Box>
       <Flex direction="column" gridGap={6}>
